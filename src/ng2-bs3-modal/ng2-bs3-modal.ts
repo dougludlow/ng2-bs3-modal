9fd9592ad04729985c3d007faa9ef62861fc183c
Douglas Ludlow <doug.ludlow@royaljay.net>
2016-03-23 10:36:51 -0600

    Abstract to ModalInstance. Use observables. Distinguish close and dismiss events.

--- src/ng2-bs3-modal/ng2-bs3-modal.ts
+++ src/ng2-bs3-modal/ng2-bs3-modal.ts
@@ -9,6 +9,7 @@ export * from './components/modal';
 export * from './components/modal-header';
 export * from './components/modal-body';
 export * from './components/modal-footer';
+export * from './components/modal-instance';
 
 export const MODAL_DIRECTIVES: Type[] = [
     ModalComponent,
