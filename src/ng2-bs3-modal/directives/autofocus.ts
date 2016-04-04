1de53becb853d92c1221847e4a7d2985f7772139
Douglas Ludlow <doug.ludlow@royaljay.net>
2016-04-04 11:40:07 -0600

    Update ignore and clean.

--- src/ng2-bs3-modal/directives/autofocus.ts
+++ src/ng2-bs3-modal/directives/autofocus.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, OnInit } from 'angular2/core';
+import { Directive, ElementRef } from 'angular2/core';
 import { ModalComponent } from '../components/modal';
 
 @Directive({
     selector: '[autofocus]'
 })
-export class AutofocusDirective implements OnInit {
+export class AutofocusDirective {
     constructor(private el: ElementRef, private modal: ModalComponent) {
         this.modal.onOpen.subscribe(() => {
             this.el.nativeElement.focus();
