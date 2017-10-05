241d344adbff76a5560cdc34ebcfac7530d54c6d
Douglas Ludlow <doug.ludlow@royaljay.net>
2017-10-05 10:33:27 -0600

    Add sourcemap for umd bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,28 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
+const globals = {
+    // Angular dependencies
+    '@angular/core': 'ng.core',
+    '@angular/common': 'ng.common',
+
+    // Rxjs dependencies
+    'rxjs/Observable': 'Rx',
+    'rxjs/Subject': 'Rx',
+    'rxjs/add/observable/fromEvent': 'Rx.Observable',
+    'rxjs/add/observable/merge': 'Rx.Observable',
+    'rxjs/add/observable/of': 'Rx.Observable',
+    'rxjs/add/observable/zip': 'Rx.Observable',
+    'rxjs/add/operator/do': 'Rx.Observable.prototype',
+    'rxjs/add/operator/filter': 'Rx.Observable.prototype',
+    'rxjs/add/operator/map': 'Rx.Observable.prototype',
+    'rxjs/add/operator/partition': 'Rx.Observable.prototype',
+    'rxjs/add/operator/share': 'Rx.Observable.prototype',
+    'rxjs/add/operator/startWith': 'Rx.Observable.prototype',
+    'rxjs/add/operator/take': 'Rx.Observable.prototype',
+    'rxjs/add/operator/toPromise': 'Rx.Observable.prototype'
+}
 
 export default {
     input: 'ng2-bs3-modal.js',
@@ -7,7 +30,7 @@ export default {
         file: 'bundles/ng2-bs3-modal.umd.js', // output a single application bundle
         format: 'umd'
     },
-    sourceMap: false,
+    sourceMap: true,
     name: 'ng2-bs3-modal',
     onwarn: function (warning) {
         // Skip certain warnings
@@ -24,51 +47,15 @@ export default {
         // console.warn everything else
         console.warn(warning.message);
     },
-    external: [
-        '@angular/core',
-        '@angular/common',
-        'rxjs/Observable',
-        'rxjs/Subject',
-        'rxjs/add/observable/fromEvent',
-        'rxjs/add/observable/merge',
-        'rxjs/add/observable/of',
-        'rxjs/add/observable/zip',
-        'rxjs/add/operator/do',
-        'rxjs/add/operator/filter',
-        'rxjs/add/operator/map',
-        'rxjs/add/operator/partition',
-        'rxjs/add/operator/share',
-        'rxjs/add/operator/startWith',
-        'rxjs/add/operator/take',
-        'rxjs/add/operator/toPromise'
-    ],
-    globals: {
-        // Angular dependencies
-        '@angular/core': 'ng.core',
-        '@angular/common': 'ng.common',
-
-        // Rxjs dependencies
-        'rxjs/Observable': 'Rx',
-        'rxjs/Subject': 'Rx',
-        'rxjs/add/observable/fromEvent': 'Rx.Observable',
-        'rxjs/add/observable/merge': 'Rx.Observable',
-        'rxjs/add/observable/of': 'Rx.Observable',
-        'rxjs/add/observable/zip': 'Rx.Observable',
-        'rxjs/add/operator/do': 'Rx.Observable.prototype',
-        'rxjs/add/operator/filter': 'Rx.Observable.prototype',
-        'rxjs/add/operator/map': 'Rx.Observable.prototype',
-        'rxjs/add/operator/partition': 'Rx.Observable.prototype',
-        'rxjs/add/operator/share': 'Rx.Observable.prototype',
-        'rxjs/add/operator/startWith': 'Rx.Observable.prototype',
-        'rxjs/add/operator/take': 'Rx.Observable.prototype',
-        'rxjs/add/operator/toPromise': 'Rx.Observable.prototype'
-    },
+    globals: globals,
+    external: Object.keys(globals),
     plugins: [
         resolve({
             jsnext: true,
             main: true,
             browser: true
         }),
-        commonjs()
+        commonjs(),
+        sourcemaps()
     ]
 }
